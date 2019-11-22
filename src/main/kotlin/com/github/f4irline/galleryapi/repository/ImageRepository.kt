c8080641892e1e1afdc7ac65b22b7cb7e32320f8
Tommi Lepola <lepola.tommi@gmail.com>
2019-11-22 20:00:26 +0200

    Remove redundant repository methods

--- src/main/kotlin/com/github/f4irline/galleryapi/repository/ImageRepository.kt
+++ src/main/kotlin/com/github/f4irline/galleryapi/repository/ImageRepository.kt
@@ -5,7 +5,5 @@ import com.github.f4irline.galleryapi.entity.User
 import org.springframework.data.repository.CrudRepository
 
 interface ImageRepository : CrudRepository<Image, Long> {
-    fun findByPath(text: String): Image
-    fun findFirstByPath(text: String): Image
     fun findByUser(user: User): List<Image>
 }
--- src/main/kotlin/com/github/f4irline/galleryapi/repository/UserRepository.kt
+++ src/main/kotlin/com/github/f4irline/galleryapi/repository/UserRepository.kt
@@ -5,6 +5,5 @@ import org.springframework.data.repository.CrudRepository
 import java.util.*
 
 interface UserRepository : CrudRepository<User, Long> {
-    fun findByName(name: String): User?
     fun findByToken(token: UUID): User?
 }
