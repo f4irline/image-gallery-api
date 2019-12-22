263c244c97b64f8c25952bc6c2da12b813065bae
Tommi Lepola <lepola.tommi@gmail.com>
2019-12-22 15:11:06 +0200

    Change table name because of the reserved user word in PostgreSQL

--- src/main/kotlin/com/github/f4irline/galleryapi/entity/User.kt
+++ src/main/kotlin/com/github/f4irline/galleryapi/entity/User.kt
@@ -4,10 +4,11 @@ import java.util.*
 import javax.persistence.*
 
 @Entity
+@Table(name = "GALLERY_USER")
 class User (
         val name: String,
         val token: UUID = UUID.randomUUID(),
-        @OneToMany(cascade = [CascadeType.ALL], mappedBy = "user")
+        @OneToMany(cascade = [CascadeType.ALL], mappedBy = "gallery_user")
         val imageList: MutableSet<Image> = mutableSetOf(),
         @Id @GeneratedValue val userId: Long? = null
 )
